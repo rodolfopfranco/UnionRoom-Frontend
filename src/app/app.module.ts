83a63b56f3db078fc0843872f18f931394ae772b
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-28 18:43:25 -0300

    Created Room Service and Components (create, details, list, update)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule,
