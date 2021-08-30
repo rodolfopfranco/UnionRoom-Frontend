badf677cb6c3398aa29e989363792be4533ca5a0
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-30 11:56:09 -0300

    Developed update-room Component

--- src/app/update-room/update-room.component.ts
+++ src/app/update-room/update-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../room';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-update-room',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-room.component.css']
 })
 export class UpdateRoomComponent implements OnInit {
+  id:number;
+  room:Room;
+  submitted=false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private roomService:RoomService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.room = new Room();
+    this.id = this.route.snapshot.params['id'];
+    this.roomService.getRoom(this.id)
+    .subscribe(data=>{
+      console.log(data);
+      this.room  = data;
+    }, error => console.log(error));
+  }
+
+  updateRoom(){
+    this.roomService.updateRoom(this.id, this.room)
+    .subscribe(data => console.log(data), error => console.log(error));
+    this.room = new Room();
+    this.gotoList();
+  }
+
+  onSubmit(){
+    this.updateRoom();
+  }
+
+  gotoList(){
+    this.router.navigate(['/rooms']);
   }
 
 }
